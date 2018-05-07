fe53f087d067fc577d31adf812be8c7367f82510
Neries <0neries0@gmail.com>
2018-05-07 14:07:12 +0300

    fix buttons in navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,16 +4,15 @@
 
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ route('home') }}"><span class="glyphicon glyphicon-home"
-                                                     aria-hidden="true"></span> Home</a></li>
-                <li><a href="{{ route('employees') }}">
+                <li class="{{ Request::route()->getName() == 'home' ? 'active' : '' }}"><a href="{{ route('home') }}"><span class="glyphicon glyphicon-home"
+                                                                                                                            aria-hidden="true"></span> Home</a></li>
+                <li class="{{ Request::route()->getName() == 'employees' ? 'active' : '' }}"><a href="{{ route('employees') }}">
                         <span class="glyphicon  glyphicon-user" aria-hidden="true"></span> All</a></li>
 
-                <li><a href="{{ route('createForm') }}">
+                <li class="{{ Request::route()->getName() == 'createForm' ? 'active' : '' }}"><a href="{{ route('createForm') }}">
                         <span class="glyphicon  glyphicon-plus" aria-hidden="true"></span> Create</a></li>
 
                 <li><a href="//www.work.ua/resumes/4774041/"><span class="glyphicon glyphicon-envelope"></span> Contact</a></li>
-
 
             </ul>
 
